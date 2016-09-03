98c1d01abf7dd1acd1d3bc96896c52853fc58024
plantoteachme <amyplant@me.com>
2016-09-03 13:14:15 -0500

    Add cool new shiny app icon

---
+++ DerivedData/checkMates/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-checkMates.build/DerivedSources/Pods_checkMates_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_checkMatesVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_checkMates  PROJECT:Pods-1" "\n"; const double Pods_checkMatesVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/checkMates/Build/Intermediates/Pods.build/Debug-iphonesimulator/Pods-checkMatesTests.build/DerivedSources/Pods_checkMatesTests_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char Pods_checkMatesTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_checkMatesTests  PROJECT:Pods-1" "\n"; const double Pods_checkMatesTestsVersionNumber __attribute__ ((used)) = (double)1.;
