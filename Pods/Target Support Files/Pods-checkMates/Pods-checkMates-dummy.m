41387917e91350a7936b3d05696efcb115bfa882
“plantoteachme” <amyplant@me.com>
2016-09-04 14:52:38 -0500

    Fix some broken dependencies and recreated the pod file

--- Pods/Target Support Files/Pods-checkMates/Pods-checkMates-dummy.m
+++ Pods/Target Support Files/Pods-checkMates/Pods-checkMates-dummy.m
@@ -1,5 +1,5 @@
 #import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_checkMates : NSObject
+@interface PodsDummy_Pods_CheckMates : NSObject
 @end
-@implementation PodsDummy_Pods_checkMates
+@implementation PodsDummy_Pods_CheckMates
 @end
--- Pods/Target Support Files/Pods-checkMates/Pods-checkMates-umbrella.h
+++ Pods/Target Support Files/Pods-checkMates/Pods-checkMates-umbrella.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
 
-FOUNDATION_EXPORT double Pods_checkMatesVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_checkMatesVersionString[];
+FOUNDATION_EXPORT double Pods_CheckMatesVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_CheckMatesVersionString[];
 
